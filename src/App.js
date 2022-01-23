f4aa4569e4cb662d609556ee35103f91b9266c41
ANNAMALAI357 <60737606+ANNAMALAI357@users.noreply.github.com>
2022-01-23 17:03:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import X from './Day3/Assignment_3/X';
-import Y from './Day3/Assignment_3/Y';
-import Z from './Day3/Assignment_3/Z';
-
 
 function App() {
   return (
-    <>
-   <X a={10} b={23}/>
-   <Y a={15} b={25} c={34}/>
-   <Z/>
-  </>
+  
   )
 }
 
